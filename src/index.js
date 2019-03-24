317d02152903fabab79c9ec12d86ebb15596d6d3
bluessista <inga@bluessista.com>
2019-03-24 17:57:05 +0100

    feat: add middleware to app

--- src/index.js
+++ src/index.js
@@ -7,16 +7,12 @@ import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import middleware from './middleware';
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, 
     document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
