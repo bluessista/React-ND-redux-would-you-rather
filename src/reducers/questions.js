6f3bf41e6f181a8cc143cb6467dd783758e52dfe
bluessista <inga@bluessista.com>
2019-03-24 17:57:41 +0100

    feat: update reducers

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions';
+import { RECEIVE_QUESTIONS, ADD_QUESTION, SAVE_ANSWER } from '../actions/questions';
 
 export default function questions (state = {}, action) {
     switch(action.type) {
@@ -6,6 +6,27 @@ export default function questions (state = {}, action) {
             return {
                 ...state,
                 ...action.questions
+            };
+        case ADD_QUESTION :
+            const { question } = action;
+            return {
+                ...state,
+                [question.id]: question,
+            };
+        case SAVE_ANSWER :
+            const { object } = action;
+            return {
+                ...state,
+                [object.qid]: {
+                    ...state[object.qid],
+                    [object.answer]: {
+                        ...state[object.qid][object.answer],
+                        votes: [
+                            ...state[object.qid][object.answer].votes,
+                            object.authedUser
+                        ]
+                    }
+                }
             };
         default :
             return state;
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,5 @@
 import { RECEIVE_USERS } from '../actions/users';
+import { ADD_QUESTION, SAVE_ANSWER } from '../actions/questions';
 
 export default function users (state = {}, action) {
     switch(action.type) {
@@ -6,6 +7,29 @@ export default function users (state = {}, action) {
             return {
                 ...state,
                 ...action.users
+            };
+        case ADD_QUESTION:
+            const { question } = action;
+            return {
+                ...state,
+                [question.author]: {
+                    ...state[question.author],
+                    questions: state[question.author].questions.concat([
+                        question.id
+                    ])
+                }
+            };
+        case SAVE_ANSWER:
+            const { object } = action;
+            return {
+                ...state,
+                [object.authedUser]: {
+                    ...state[object.authedUser],
+                    answers: {
+                        ...state[object.authedUser].answers,
+                        [object.qid]: object.answer
+                    }
+                }
             };
         default :
             return state;
